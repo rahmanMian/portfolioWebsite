de25fbd9dfa8ffa113103a397edf5911629bd6e9
rahmanMian <rahmanakbarmian@gmail.com>
2024-08-11 00:10:15 -0600

    projects up need to mod them

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -45,7 +45,7 @@ const Experience = () => {
   return (
      <>
      <motion.div variants={textVariant()}>
-     <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+     <h2 className={styles.sectionHeadText}>Work Experience</h2>
      </motion.div>
 
      <div className="mt-20 flex flex-col">
